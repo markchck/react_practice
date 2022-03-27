70ab182d9137412e4fc5d3284c17170963a2bc2a
Jaemin Ki <markchck@gmail.com>
2022-03-27 14:28:38 +0900

    Create, Read 완료

--- src/App.js
+++ src/App.js
@@ -37,37 +37,36 @@ function Article(props){
 }
 
 function Create(props){
-    return <form onSubmit={(event)=>{
-      event.preventDefault();
-      const _title = event.target.title.value
-      const _body = event.target.body.value
-      // 아래 굉장히 중요한 순간임..(사용자정의 태그에서 function으로는 props를 통해 정보를 보내면 되는데 그 반대로 function에서 
-      // 사용자 정의 태그로 정보를 보낼 때는 props.onCreate(title,body)처럼 보낼 정보를 정의를 해서 보낸다.)
-      props.onCreate(_title, _body)     
-    }}>
-      <input type="text" name="title"></input>
-      <input type="text" name="body"></input>
-      <input type="submit"></input>
-    </form>
+    return <article>
+      <h2>Create</h2>
+      <form onSubmit={(event)=>{
+        event.preventDefault();
+        const _title = event.target.title.value
+        const _body = event.target.body.value
+        // 아래 굉장히 중요한 순간임..(사용자정의 태그에서 function으로는 props를 통해 정보를 보내면 되는데 그 반대로 function에서 
+        // 사용자 정의 태그로 정보를 보낼 때는 props.onCreate(title,body)처럼 보낼 정보를 정의를 해서 보낸다.)
+        props.onCreate(_title, _body)     
+      }}>
+        <input type="text" name="title"></input>
+        <input type="text" name="body"></input>
+        <input type="submit"></input>
+      </form>
+    </article> 
   }
 
 function App() {
   const [mode, setMode] = useState('Welcome')
   const [id, setId] = useState(null)
   const [nextId, setNextId] = useState(3)
-  // const [topics, setTopics] =useState(
-  //   [
-  //     {id: 1, title: 'html', body: 'html is ...'},
-  //     {id: 2, title: 'Csss', body: 'Csss is ...'},
-  //     {id: 3, title: 'Javascript', body: 'javascript is ...'}
-  //   ]
-  // )
 
-  const topics = [
-        {id: 0, title: 'html', body: 'html is ...'},
-        {id: 1, title: 'Csss', body: 'Csss is ...'},
-        {id: 2, title: 'Javascript', body: 'javascript is ...'}
-      ]
+  const [topics, setTopics] =useState(
+    [
+      {id: 0, title: 'html', body: 'html is ...'},
+      {id: 1, title: 'Csss', body: 'Csss is ...'},
+      {id: 2, title: 'Javascript', body: 'javascript is ...'}
+    ]
+  )
+
 
   let content = null;
   if(mode === 'Welcome'){
@@ -76,25 +75,12 @@ function App() {
     content = <Article title={topics[id].title} body={topics[id].body}></Article>
   }else if(mode === 'Create'){
     content = <Create onCreate={(_title,_body)=>{
-      const title = _title
-      const body = _body
-
-      //id 를 삽입하는 방법?
       topics.push({id: nextId, title: _title, body: _body})
-      console.log(topics)
-      setId(nextId)
+      const newTopics = topics
+      setTopics(newTopics)
       setMode('Read')
+      setId(nextId)
       setNextId(nextId+1)
-      
-      
-      // const newTopic = {id: nextId, title: _title, body: _body}
-      // const newTopics = [...topics]
-      // newTopics.push(newTopic)
-      // setTopics(newTopics)
-      // setMode('Read')
-      // setLi(nextId)
-      // setNextId(nextId+1)
-    
     }}></Create>
   }
 
@@ -112,7 +98,7 @@ function App() {
       <a href='/create' onClick={(event)=>{
         event.preventDefault();
         setMode('Create')
-      }}>Create-1</a>
+      }}>Create</a>
     </div>
   );
 }
